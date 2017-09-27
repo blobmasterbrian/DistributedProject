bdd3c64b11f9aae1393389d2c8c4d09e67013a69
charliepigottNYU <cdp332@nyu.edu>
2017-09-27 13:51:14 -0400

    initial go server code

---
+++ src/server.go
@@ -0,0 +1,23 @@
+package main
+
+import(
+    "fmt"
+    "net/http"
+)
+
+
+
+func main(){
+    http.HandleFunc("/signupsubmit", signup)
+    http.ListenAndServe(":8080", nil)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        r.ParseForm()
+        fmt.Fprintf(w, "username check: %s, password check: %s\n", r.PostFormValue("username"),
+            r.PostFormValue("password"))
+    }
+}
+
+
