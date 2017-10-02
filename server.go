8d2d0e4f2962b87073007dd4166031bc3c3ebd81
Brian Quinn <bjq207@nyu.edu>
2017-10-02 17:56:37 -0400

    Added html loading for server URLs

--- server.go
+++ server.go
@@ -10,29 +10,52 @@ import(
 var USERS map[string]src.UserInfo
 
 func main(){
+    http.HandleFunc("/", welcomeRedirect)
+    http.HandleFunc("/welcome", welcome)
+    http.HandleFunc("/signup", signup)
+    http.HandleFunc("/login", login)
     http.HandleFunc("/home",home)
-    http.HandleFunc("/signupsubmit", signup)
+
+    http.HandleFunc("/signup-response", signupResponse)
+    http.HandleFunc("/login-response", loginResponse)
     http.ListenAndServe(":8080", nil)
 }
 
+func welcomeRedirect(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, "/welcome", http.StatusPermanentRedirect)
+}
+
 func welcome(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "web/welcome.html")
 }
 
 func signup(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodPost {
-        r.ParseForm()
-        fmt.Fprintf(w, "username check: %s, password check: %s\n", r.PostFormValue("username"),
-            r.PostFormValue("password"))
-    }
+    http.ServeFile(w, r, "web/signup.html")
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-    if r.Method == http.MethodPost {
-        r.ParseForm()
-    }
+    http.ServeFile(w, r, "web/login.html")
 }
 
 func home(w http.ResponseWriter, r *http.Request){
     t, _ := template.ParseFiles("web/homepage.html")
     t.Execute(w, "Charlie")
+}
+
+func signupResponse(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        r.ParseForm()
+        http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
+        fmt.Printf("Username: %s, Password: %s, Confirmed Pass: %s\n", r.PostFormValue("username"),
+            r.PostFormValue("password"), r.PostFormValue("confirm"))
+    }
+}
+
+func loginResponse(w http.ResponseWriter, r *http.Request) {
+    if r.Method == http.MethodPost {
+        r.ParseForm()
+        http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
+        fmt.Printf("Username: %s, Password: %s\n", r.PostFormValue("username"),
+            r.PostFormValue("password"))
+    }
 }
