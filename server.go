a5a1be381e039b2415834655b3f057e8e3a85fae
Brian Quinn <bjq207@nyu.edu>
2017-10-02 21:17:06 -0400

    Added error page/demonstration of templating

--- server.go
+++ server.go
@@ -15,6 +15,7 @@ func main(){
     http.HandleFunc("/signup", signup)
     http.HandleFunc("/login", login)
     http.HandleFunc("/home",home)
+    http.HandleFunc("/error", errorPage)
 
     http.HandleFunc("/signup-response", signupResponse)
     http.HandleFunc("/login-response", loginResponse)
@@ -37,14 +38,23 @@ func login(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, "web/login.html")
 }
 
-func home(w http.ResponseWriter, r *http.Request){
+func home(w http.ResponseWriter, r *http.Request) {
     t, _ := template.ParseFiles("web/homepage.html")
     t.Execute(w, "Charlie")
+}
+
+func errorPage(w http.ResponseWriter, r *http.Request) {
+    t, _ := template.ParseFiles("web/error.html")
+    t.Execute(w, struct {Name string; Error string}{Name: "Dave", Error: "Singularity"})
 }
 
 func signupResponse(w http.ResponseWriter, r *http.Request) {
     if r.Method == http.MethodPost {
         r.ParseForm()
+        if r.PostFormValue("password") != r.PostFormValue("confirm") {
+            http.Redirect(w, r, "/error", http.StatusPermanentRedirect)
+            return
+        }
         http.Redirect(w, r, "/home", http.StatusPermanentRedirect)
         fmt.Printf("Username: %s, Password: %s, Confirmed Pass: %s\n", r.PostFormValue("username"),
             r.PostFormValue("password"), r.PostFormValue("confirm"))
