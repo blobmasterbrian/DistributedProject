ef2d13576ed1213bacdaea1b1acb7ae3b5a51ae2
charliepigottNYU <cdp332@nyu.edu>
2017-10-04 17:39:58 -0400

    follow method

--- src/UserInfo.go
+++ src/UserInfo.go
@@ -14,6 +14,22 @@ func NewUserInfo(username, password string) *UserInfo {
     return newUser
 }
 
-func (u *UserInfo) CheckPass(password string) bool{
+func (u *UserInfo) CheckPass(password string) bool {
     return u.password == password
+}
+
+func (user *UserInfo) Follow(newFollow *UserInfo) bool {
+    if newFollow == nil || user.following[newFollow.Username] != nil {
+        return false
+    }
+    user.following[newFollow.Username] = newFollow
+    return true
+}
+
+func (user *UserInfo) UnFollow(oldFollow *UserInfo) bool {
+    if oldFollow == nil || user.following[oldFollow.Username] == nil {
+        return false
+    }
+    user.following[oldFollow.Username] = nil
+    return true
 }
