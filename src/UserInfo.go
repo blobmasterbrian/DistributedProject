8ba2262d38e673d208df3892044813f82b7cd49d
charliepigottNYU <cdp332@nyu.edu>
2017-10-04 20:06:55 -0400

    start of posts

--- src/UserInfo.go
+++ src/UserInfo.go
@@ -1,9 +1,21 @@
 package src
 
+import (
+    "time"
+    "container/heap"
+)
+
+
 type UserInfo struct {
 	Username string
 	password string
 	following map[string]*UserInfo
+    posts []post
+}
+
+type post {
+    stamp Time
+    message string
 }
 
 func NewUserInfo(username, password string) *UserInfo {
@@ -32,4 +44,14 @@ func (user *UserInfo) UnFollow(oldFollow *UserInfo) bool {
     }
     user.following[oldFollow.Username] = nil
     return true
+}
+
+func (user *UserInfo) WritePost(msg string){
+    newPost := post{message: msg, stamp: time.Now()}
+    append(user.posts, newPost)
+}
+
+func (user *UserInfo) GetAllChirps() []post {
+    result []post
+    return result
 }
