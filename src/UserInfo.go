e5aaa86f6a9dd9d162d7ab5521061f7a386b4007
charliepigottNYU <cdp332@nyu.edu>
2017-10-04 21:42:36 -0400

    queue for printing posts in order

--- src/UserInfo.go
+++ src/UserInfo.go
@@ -13,17 +13,48 @@ type UserInfo struct {
     posts []post
 }
 
-type post {
-    stamp Time
-    message string
-}
-
 func NewUserInfo(username, password string) *UserInfo {
     newUser := new(UserInfo)
     newUser.Username = username
     newUser.password = password
     newUser.following = make(map[string]*UserInfo)
     return newUser
+}
+
+type post struct {
+    stamp time.Time
+    message string
+    index int       //index of post in priority queue
+}
+
+type PriorityQueue []*post
+
+func (q PriorityQueue) Len() int {return len(q)}
+
+func (q PriorityQueue) Less(i, j int) bool {
+    return q[i].stamp.Before(q[j].stamp)
+}
+
+func (q PriorityQueue) Swap(i,j int) {
+    q[i], q[j] = q[j], q[i]
+    q[i].index = i
+    q[j].index = j
+}
+
+func (q *PriorityQueue) Push(x interface{}){
+    newLen := len(*q)
+    newPost := x.(*post)
+    newPost.index = newLen
+    *q = append(*q, newPost)
+}
+
+func (q *PriorityQueue) Pop() interface{} {
+    oldQ := *q
+    n := len(oldQ)
+    removedPost := oldQ[n-1]
+    removedPost.index = -1
+    *q = oldQ[0: n-1]
+    return removedPost
 }
 
 func (u *UserInfo) CheckPass(password string) bool {
@@ -48,10 +79,18 @@ func (user *UserInfo) UnFollow(oldFollow *UserInfo) bool {
 
 func (user *UserInfo) WritePost(msg string){
     newPost := post{message: msg, stamp: time.Now()}
-    append(user.posts, newPost)
+    user.posts = append(user.posts, newPost)
 }
 
 func (user *UserInfo) GetAllChirps() []post {
-    result []post
+    var result []post
+
+    allChirps := make(PriorityQueue, 10)
+    heap.Init(&allChirps)
+    for _, followed := range user.following {
+        for _, singlePost := range followed.posts {
+            heap.Push(&allChirps,singlePost)
+        }
+    }
     return result
 }
