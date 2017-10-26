2cdfbadee2039d69563f07c22092b77b033644fb
Brian Quinn <bjq207@nyu.edu>
2017-10-26 17:03:51 -0400

    Added CommandRequest struct for uniform gobbing

--- lib/StatusCodes.go
+++ lib/StatusCodes.go
@@ -29,7 +29,7 @@ const (
 var statusText = map[int]string {
 	StatusAccepted:       "Command Accepted and Executed Successfully",
 	StatusUserFound:      "User Found",
-	StatusUserNotFound:  "User Does Not Exist",
+	StatusUserNotFound:   "User Does Not Exist",
 	StatusUserFollowed:   "User Followed",
 	StatusUserUnfollowed: "User Not Followed",
 	StatusEmpty:          "Object is Empty",
@@ -43,8 +43,13 @@ func StatusText(code int) string {
 	return statusText[code]
 }
 
+type CommandRequest struct {
+	CommandCode  int
+	Data         interface{}
+}
+
 type CommandResponse struct {
-	Success bool
-	Status  int
-	Value   interface{}
+	Success  bool
+	Status   int
+	Data     interface{}
 }
